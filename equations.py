296e0230658a566ce0c293cddec1e6bfa521bb33
Jorgedavyd <jorged.encyso@gmail.com>
2025-01-19 13:33:31 -0300

    default

--- equations.py
+++ equations.py
@@ -10,9 +10,15 @@ class HamiltonianBodies(PDE):
     def __init__(self, masses: List[float]) -> None:
         super().__init__()
         assert len(masses) == 3, "3 masses only"
-        q1, q2, q3, q4, q5, q6 = [Symbol(f"q{i}") for i in range(1, 7)]
         m1, m2, m3 = masses
         t = Symbol("t")
+        q1, q2, q3, q4, q5, q6 = [Symbol(f"q{i}") for i in range(1, 7)]
+        p1, p2, p3, p4, p5, p6 = q1.diff(t) * m1, q2.diff(t) * m1, q3.diff(t) * m2, q4.diff(t) * m2, q5.diff(t) * m3, q6.diff(t) * m3
+        H = Function()(*[f"q{i}" for i in range(1, 7)])
+        self.equations = dict(
+            "first": (p1 / 2 * m1) - ...,
+            "second": (p1 / 2 * m1) - ...,
+        )
         # self.equations = dict(
         #    "x": 1
         # )
