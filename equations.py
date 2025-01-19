9243defb764368d35e8de3e3240dbeb3d916b98e
Jorgedavyd <jorged.encyso@gmail.com>
2025-01-19 11:56:52 -0300

    default

--- equations.py
+++ equations.py
@@ -14,11 +14,10 @@ class HamiltonianBodies(PDE):
         t = Symbol("t")
         q1, q2, q3, q4, q5, q6 = [Symbol(f"q{i}") for i in range(1, 7)]
         p1, p2, p3, p4, p5, p6 = q1.diff(t) * m1, q2.diff(t) * m1, q3.diff(t) * m2, q4.diff(t) * m2, q5.diff(t) * m3, q6.diff(t) * m3
+        q: List[Symbol] = [q1, q2, q3, q4, q5, q6]
+        p: List[Symbol] = [p1, p2, p3, p4, p5, p6]
         H = Function()(*[f"q{i}" for i in range(1, 7)])
         self.equations = dict(
-            "first": (p1 / 2 * m1) - ...,
-            "second": (p1 / 2 * m1) - ...,
+            "q_dot_hamiltonian": p / 2 * m - G * U(q),
+            "p_dot_hamiltonian": p / 2 * m - G * U(q),
         )
-        # self.equations = dict(
-        #    "x": 1
-        # )
--- train.py
+++ train.py
@@ -1,7 +1,5 @@
-from sympy import Symbol
-
+from typing import List
 import modulus.sym
-from modulus.sym.hydra import ModulusConfig
 from modulus.sym.solver import Solver
 from modulus.sym.domain import Domain
 from modulus.sym.domain.constraint import (
@@ -9,57 +7,54 @@ from modulus.sym.domain.constraint import (
     PointwiseInteriorConstraint,
     IntegralBoundaryConstraint,
 )
-from model import PhysicsInformedModel
 from equations import HamiltonianBodies
 from modulus.sym.key import Key
-
-from modulus.sym.geometry.parametrization import Parametrization, Parameter
-from modulus.sym.geometry.primitives_3d import Box, Sphere, Cylinder, Plane
-from modulus.sym.geometry.primitives_2d import Rectangle, Circle, Line
-from modulus.sym.geometry.primitives_1d import Point1D, Line1D
-from modulus.sym.geometry.tessellation import Tessellation
-from modulus.sym.utils.io.vtk import var_to_polyvtk
-
+from modulus.sym.geometry.primitives_2d import Rectangle
+from modulus.sym.hydra import instantiate_arch, ModulusConfig
 
 def make_geometry():
-    return ...
+    return Rectangle()
 
 
 @modulus.sym.main(version_base="1.3", config_path="conf", config_name="config")
 def run(cfg: ModulusConfig) -> None:
-    r = Symbol("r")  # sample inputs
-    t = Symbol("t")
-    eq = HamiltonianBodies()
+    m: List[float] = None
+    eq = HamiltonianBodies(*m)
+    h_net = instantiate_arch(
+        input_keys=[Key(f"q{i}") for i in range(1, 7)] + [Key(f"p{i}") for i in range(1, 7)] + [Key("t")],
+        output_keys=[Key("H")],
+        cfg=cfg.arch.fully_connected,
+    )
 
-    u_net = PhysicsInformedModel(input_keys=[Key("x")], output_keys=[Key("u")], cfg=cfg)
-
-    nodes = eq.make_nodes() + [u_net.make_node(name="model")]
+    nodes = eq.make_nodes() + [h_net.make_node(name="model")]
     geo = make_geometry()
     domain = Domain()
 
     boundary = PointwiseBoundaryConstraint(
-        nodes=nodes, geometry=geo, outvar={"E": 0}, batch_size=2
+        nodes=nodes, geometry=geo, outvar={"H": 0}, batch_size=cfg.batch_size.boundary
     )
 
-    interior = PointwiseInteriorConstraint(
+    q_dot = PointwiseInteriorConstraint(
         nodes=nodes,
         geometry=geo,
-        outvar={"CustomPDE": 0},
-        batch_size=100,
-        bounds={r: (0, 210), t: (0, 7200)},
+        outvar={"q_dot": 0},
+        batch_size=cfg.batch_size.q_dot,
+        bounds={
+        },
     )
 
-    integral = IntegralBoundaryConstraint(
+    p_dot = PointwiseInteriorConstraint(
         nodes=nodes,
         geometry=geo,
-        outvar={"CustomPDE": 0},
-        batch_size=1,
-        integral_batch_size=1000,
-    )  ## monte carlo integration for this one
+        outvar={"p_dot": 0},
+        batch_size=cfg.batch_size.p_dot,
+        bounds={
+        },
+    )
 
-    domain.add_constraint(boundary, "bc1")
-    domain.add_constraint(integral, "bc2")
-    domain.add_constraint(interior, "PDE")
+    domain.add_constraint(boundary, "boundary")
+    domain.add_constraint(p_dot, "p_dot_hamiltonian")
+    domain.add_constraint(q_dot, "q_dot_hamiltonian")
 
     slv = Solver(cfg, domain)
     slv.solve()
